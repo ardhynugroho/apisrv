d963142130012a7ad680414a66628888cfdb624c
Ardhy Nugroho <ardhy.wisnu.nugroho@gmail.com>
2024-10-31 15:18:31 +0700

    recreate

--- auth.py
+++ auth.py
@@ -1,6 +1,13 @@
 import functools
-from flask import Blueprint
-from flasgger.utils import swag_from
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from db import get_db
+
+from flasgger import swag_from
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -8,7 +15,6 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @swag_from("oas/auth.login.get.yaml", methods=['GET'])
 @swag_from("oas/auth.login.post.yaml", methods=['POST'])
 def login():
-    
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -46,3 +52,48 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
